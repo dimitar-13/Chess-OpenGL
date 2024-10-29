aaa41b6003775fc463c8a279763a32156bbb2cc0
Laptop <dimitarnaidenov5@gmail.com>
2024-10-29 11:04:04 +0200

    Finishing docs.

--- src/Core/SceneObject.h
+++ src/Core/SceneObject.h
@@ -6,18 +6,39 @@ namespace Chess_Game
 {
     class Application;
 
+    /**
+     * @brief Base class for creating a scene.
+     *
+     * This is mainly here for ease of testing since every scene is easier to test separately.
+     */
     class SceneObject
     {
     public:
+        /**
+         * @brief Constructor uses Dependency Injection(DI) to resolve the application class dependency. 
+         */
         SceneObject(std::weak_ptr<Application> main_application) :
             m_Application(main_application)
         {}
+        /**
+         * @brief Virtual function called when scene must be initialized. 
+         */
         virtual void InitScene() = 0;
-        virtual void DrawScene(std::shared_ptr<BatchRenderer> application_batch_renderer) = 0;
+        /**
+         * @brief Virtual function for drawing the scene. 
+         * @param application_batch_renderer The batch renderer to use for drawing.
+         */
+        virtual void DrawScene(std::shared_ptr<BatchRenderer> application_batch_renderer) { };
+        /**
+         * @brief Virtual function called every frame update. 
+         */
         virtual void OnUpdate() = 0;
-        virtual void DestroyScene() = 0;
+        /**
+         * @brief Virtual function for destroying the scene. 
+         */
+        virtual void DestroyScene() { };
     protected:
-        std::vector<std::weak_ptr<Drawable>> m_SceneObjects{};
-        std::weak_ptr<Application> m_Application{};
+        std::vector<std::weak_ptr<Drawable>> m_SceneObjects{};  ///< Scene drawable objects array.
+        std::weak_ptr<Application> m_Application{};             ///< Weak ref to application.
     };
 }
